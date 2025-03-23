5c98d52bc8fa985969cb820e358952eef230dc25
Spagum <ethanlee9113@gmail.com>
2025-03-23 13:55:03 +1100

    updated it

--- src/app/PastStartupEvents/page.js
+++ src/app/PastStartupEvents/page.js
@@ -40,6 +40,24 @@ export default function PastStartupEvents() {
 
 
 
+
+                    {/* Listing 1 */}
+                    <div className="relative border-2 rounded-xl items-center overflow-hidden hover:scale-105 transition transform duration-200 ease-in-out">
+                        <Image src="/s&c1.avif" width={400} height={400} className="w-full h-[275px] object-cover rounded-lg" alt="Event Image" />
+
+                        <div className=" p-5 ">
+                            <h3 className="mb-1 text-lg font-bold">Melbourne | AI Builder Co-working x S&C
+                            </h3>
+                            <p className="text-gray-400 text-sm">Fri, Mar 21, 9:00 AM - 5:00 PM
+
+                            </p>
+                            <p className="text-gray-400 text-sm">Stone & Chalk Melbourne Startup Hub</p>
+                        </div>
+
+
+                    </div>
+
+
                     {/* Listing 1 */}
                     <div className="relative border-2 rounded-xl items-center overflow-hidden hover:scale-105 transition transform duration-200 ease-in-out">
                         <Image src="/leonardo_march.png" width={400} height={400} className="w-full h-[275px] object-cover rounded-lg" alt="Event Image" />
@@ -75,7 +93,7 @@ export default function PastStartupEvents() {
 
 
 
-                    {/* Listing 2 */}
+                    {/* Listing 3 */}
                     <div className="relative border-2 rounded-xl items-center overflow-hidden hover:scale-105 transition transform duration-200 ease-in-out">
                         <Image src="/bc_monthly_march.png" width={400} height={400} className="w-full h-[275px] object-cover rounded-lg" alt="Event Image" />
 
